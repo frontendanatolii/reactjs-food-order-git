26226f212020d9fbb992fa862ffb81792af6baf5
Tolik Petras <toliketras4@gmail.com>
2023-09-08 16:04:28 +0300

    added cart visibility

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const changeCartIsShown = () => {
+    setCartIsShown((prevState) => !prevState)
+  }
+  
   return (
     <CartProvider>
+      {cartIsShown && <div>Cart...</div>}
       <Header />
       <main>
         <Meals />
